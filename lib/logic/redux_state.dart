36f2f7f5f509980677ba90f3e2791bd2062e9e77
Marcin Szalek <marcin.szalek1@gmail.com>
2018-02-17 21:10:37 +0100

    Removed checkout state

--- lib/logic/redux_state.dart
+++ lib/logic/redux_state.dart
@@ -14,7 +14,6 @@ class ReduxState {
   final League activeLeague;
   final String activeLeagueName;
   final List<String> availableLeagueNames;
-  final CheckoutState checkoutState;
 
   ReduxState({this.mainPageState = MainPageState.HOME,
     this.sessionPageState = const SessionPageState(),
@@ -22,8 +21,7 @@ class ReduxState {
     this.activeSession,
     this.activeLeague,
     this.availableLeagueNames = const [],
-    this.activeLeagueName,
-    this.checkoutState});
+    this.activeLeagueName,});
 
   ReduxState copyWith({
     MainPageState mainPageState,
@@ -33,7 +31,6 @@ class ReduxState {
     League activeLeague,
     List<String> availableLeagueNames,
     String activeLeagueName,
-    CheckoutState checkoutState,
   }) {
     return new ReduxState(
       mainPageState: mainPageState ?? this.mainPageState,
@@ -43,7 +40,6 @@ class ReduxState {
       activeLeague: activeLeague ?? this.activeLeague,
       availableLeagueNames: availableLeagueNames ?? this.availableLeagueNames,
       activeLeagueName: activeLeagueName ?? this.activeLeagueName,
-      checkoutState: checkoutState ?? this.checkoutState,
     );
   }
 
@@ -67,72 +63,6 @@ class SessionPageState {
   SessionPageState copyWith({Map<Player, bool> playersExpanded}) {
     return new SessionPageState(
       playersExpanded: playersExpanded ?? this.playersExpanded,
-    );
-  }
-}
-
-class CheckoutSliderState {
-  final int minValue;
-  final int value;
-  final int maxValue;
-
-  CheckoutSliderState({this.minValue = 0, this.value = 0, this.maxValue = 0});
-
-  CheckoutSliderState copyWith({int minValue, int value, int maxValue}) {
-    return new CheckoutSliderState(
-      minValue: minValue ?? this.minValue,
-      value: value ?? this.value,
-      maxValue: maxValue ?? this.maxValue,
-    );
-  }
-}
-
-class CheckoutState {
-  final Player player;
-  final Session session;
-  final Map<Player, int> playerDebts;
-  final Map<Player, CheckoutSliderState> checkoutsFromDebtsSliders;
-  final CheckoutSliderState cashCheckoutSlider;
-  final CheckoutSliderState totalCheckoutSlider;
-
-  CheckoutState({this.checkoutsFromDebtsSliders,
-    this.cashCheckoutSlider,
-    this.totalCheckoutSlider,
-    this.player,
-    this.session,
-    this.playerDebts}) {
-//    if (playerDebts.isEmpty) {
-//      playerDebts.addAll(new Map<Player, int>.fromIterable(
-//          session.playerSessions.values.where((ps) => ps.debtBuyIn != 0),
-//          key: (PlayerSession ps) => ps.player,
-//          value: (PlayerSession ps) => ps.debtBuyIn));
-//    }
-//    if (checkoutsFromDebts.isEmpty) {
-//      playerDebts.forEach(
-//          (player, debt) => checkoutsFromDebts.putIfAbsent(player, () => 0));
-//    }
-  }
-
-  int get moneyDeclaredToCheckout =>
-      cashCheckoutSlider.value +
-          (checkoutsFromDebtsSliders.isEmpty
-              ? 0
-              : checkoutsFromDebtsSliders.values
-              .fold<int>(0, (a, b) => a + b.value));
-
-  int get leftResources => totalCheckoutSlider.value - moneyDeclaredToCheckout;
-
-  CheckoutState copyWith({Map<Player, CheckoutSliderState> checkoutsFromDebts,
-    CheckoutSliderState cashCheckout,
-    CheckoutSliderState totalCheckout}) {
-    return new CheckoutState(
-      checkoutsFromDebtsSliders:
-      checkoutsFromDebts ?? this.checkoutsFromDebtsSliders,
-      cashCheckoutSlider: cashCheckout ?? this.cashCheckoutSlider,
-      totalCheckoutSlider: totalCheckout ?? this.totalCheckoutSlider,
-      player: this.player,
-      session: this.session,
-      playerDebts: this.playerDebts,
     );
   }
 }
